b8135c8e3b293c931910a5486643374967bdc98d
Brutesque <alain@brutesque.com>
2016-02-27 17:17:51 -0400

    updated deployment code

--- run_update.sh
+++ run_update.sh
@@ -9,16 +9,19 @@ set -e
 # Collect data.
 #------------------------------------------------------------------------------
 
-python get_octane_scores.py
-python get_tweakers_prices.py
+#python get_octane_scores.py
+#python get_tweakers_prices.py
 python db_to_html.py
 
 
 # Update git repository and push to gh-pages.
 #------------------------------------------------------------------------------
 
-# git checkout gh-pages
+git checkout gh-pages
 mv /tmp/octane_tweakers.html ./index.html
+git commit -am "automated update"
+git push
+git checkout master
 
 
 # Finished.
